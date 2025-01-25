34e6c062449a0a23a1774b12dc124cbf39c7ce02
c-santos <jcms121899@gmail.com>
2025-01-25 18:31:51 +0800

    feat: added server scaffolding and health check endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/c-santos/go-auth/internal/config"
+	"log"
+	"net/http"
+)
+
 func main() {
+
+	port := config.LoadConfig().Port
+
+	mux := &http.ServeMux{}
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, I am running on %q", port)
+	})
+
+	log.Printf("Listening on PORT %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
