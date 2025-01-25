cbb3be2d3cd607b59d4288bd42e3aaec6206f63e
c-santos <jcms121899@gmail.com>
2025-01-25 22:37:13 +0800

    feat: added http handler for access token generation

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/c-santos/go-auth/internal/config"
 	"log"
 	"net/http"
+
+	"github.com/c-santos/go-auth/internal/auth"
+	"github.com/c-santos/go-auth/internal/config"
 )
 
 func main() {
-
 	port := config.LoadConfig().Port
 
 	mux := &http.ServeMux{}
@@ -17,6 +19,40 @@ func main() {
 		fmt.Fprintf(w, "Hello, I am running on %q", port)
 	})
 
+	mux.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL)
+
+		if r.Method != "POST" {
+			w.WriteHeader(404)
+			return
+		}
+
+		var body map[string]string
+		// Decode r.Body and put it in the memory addr of the body var
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			log.Printf("Couldn't decode request body: %s", err)
+			w.WriteHeader(400)
+			return
+		}
+
+		token, err := auth.GenerateToken(body)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		response := map[string]string{
+			"access_token": token,
+		}
+
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+	})
+
 	log.Printf("Listening on PORT %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
